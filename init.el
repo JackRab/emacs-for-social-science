d9a8d60c217bcd58c716d13242494cc013fb4b09
Yimin Yi <yyi102@syr.edu>
2017-08-08 09:45:08 -0400

    add a few packages and configurations

--- init.el
+++ init.el
@@ -13,6 +13,13 @@
 
 ;;; Code:
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (if (version< emacs-version "24.5.1"); Minimum version is emacs 24.5.1
     (error "Your Emacs is too old -- this config requires v%s or higher"))
 
@@ -20,3 +27,17 @@
 (org-babel-load-file (concat user-emacs-directory "init-emacs.org"))
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (ivy-bibtex ivy magit polymode markdown-mode auctex ess smartparens company-quickhelp company better-defaults use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
